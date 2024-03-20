6bb3344440f1940c3c15613bdd34c4a1135f1371
muhammad sani <muhammadsanijae@gmail.com>
2024-03-20 07:48:07 +0100

    update commit

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Avatar.css'
+
+export default function Avatar() {
+  return (
+    <div className='img_avatar'>
+        <img className='my_avatar'  alt='sanijae' src={require('../../assests/images/saj.jpg')} />
+    </div>
+  )
+}
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
 import { greeting } from "../../portfolio.js";
+import Avatar from "../Avatar/Avatar.js";
+//import ToggleSwitch from "./ToggleSwitch.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
   return (
     <div className="footer-div">
       <Fade>
-        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+        <div className='footer-row'>
+        <Avatar/>
+        <p className="footer-text" style={{ color: props.theme.secondaryText}}>
           {greeting.title} &nbsp; &nbsp; {new Date().getFullYear()} 
         </p>
+        </div>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
     </div>
